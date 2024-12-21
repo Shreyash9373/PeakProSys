6ee38e0e3ce5da6b31b8bbcba4efdb5fe2ad25eb
Shreyas <shreyashraut8@gmail.com>
2024-12-21 15:09:01 +0530

    Navbar Header and Services integrated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import Header from '../components/Header'
+import Header from '../components/Header.jsx'
+import Services from '../components/Services.jsx'
+import Footer from '../components/Footer.jsx'
 
 const Home = () => {
   return (
     <div>
      <Header/>
+     <Services/>
+     <Footer/>
     </div>
   )
 }
