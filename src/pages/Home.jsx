79d8abe5af4bcb73ae1156e87a46afed031519e0
anikettambe1509 <anikettambe1432@gmail.com>
2024-12-23 12:35:29 +0530

    All changes has been committed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
-// import Contact from '../components/Contact'
+import Services from '../components/Services'
+import Accordion from '../components/Accordion'
+import Footer from '../components/Footer'
+import ContactInformation from '../components/ContactInformation'
 
 const Home = () => {
   return (
     <div>
      <Header/>
-     {/* <Contact/> */}
+     <Services/>
+     <Accordion/>
+     <ContactInformation/>
+     <Footer/>
     </div>
   )
 }
