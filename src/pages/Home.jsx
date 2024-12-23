389b29ade68671b019b8f7250c9eb0c4a2244dbd
anikettambe1509 <anikettambe1432@gmail.com>
2024-12-23 11:46:38 +0530

    Updated Home.jsx with new changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Contact from '../components/Contact'
 
 const Home = () => {
   return (
     <div>
      <Header/>
+     <Contact/>
     </div>
   )
 }
